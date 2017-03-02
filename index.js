34275d06760b8e2f07ff25111a88867c01922585
Joe Hitchens <joe@sleepless.com>
2017-03-01 21:56:56 -0800

    -

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function save(data, file, name) {
 	];
 	ws['!cols'] = wscols;		// column widths */
 
-	console.log(JSON.stringify(wb));
+	//console.log(JSON.stringify(wb));
 	XLSX.writeFile(wb, file);
 }
 
